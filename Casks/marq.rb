a482a7f5a94c84cc139e210d0088a2d1f73cd94b
Jim Barritt <jim.barritt@gmail.com>
2026-03-30 23:15:47 +0100

    Update Marq to v1.1.0

--- Casks/marq.rb
+++ Casks/marq.rb
@@ -1,6 +1,6 @@
 cask "marq" do
-  version "1.0.0"
-  sha256 "acd4203d47d358c9097b01e8d88a433af80ce1d7bb88d9113da67705e1a5d77c"
+  version "1.1.0"
+  sha256 "c93080669d404eca2aaf4b02cdb797400f3c94da37c6f38fe8383a8b5332525e"
 
   url "https://github.com/jimbarritt/marq/releases/download/v#{version}/Marq.zip"
   name "Marq"
