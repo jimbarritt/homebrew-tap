46397c167966e088bf69e72cc26657e6908333b6
Jim Barritt <jim.barritt@gmail.com>
2026-05-28 17:14:39 +0100

    marq v1.2.4

--- Casks/marq.rb
+++ Casks/marq.rb
@@ -1,6 +1,6 @@
 cask "marq" do
-  version "1.2.3"
-  sha256 "4a059cc1cd671f4bcde9fbddcb38e0f8755c27e14615ede9df2c66062faf5ea3"
+  version "1.2.4"
+  sha256 "bdb835878c5e9b01614e29df1fa3317ce7f92db9c74585c8840eacef7138bab1"
 
   url "https://github.com/jimbarritt/marq/releases/download/v#{version}/Marq.zip"
   name "Marq"
