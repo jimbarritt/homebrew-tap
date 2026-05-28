3a6c561e3172c2b037da5bdc5439cbdce87f80b5
Jim Barritt <jim.barritt@gmail.com>
2026-05-28 17:26:26 +0100

    marq v1.2.5

--- Casks/marq.rb
+++ Casks/marq.rb
@@ -1,6 +1,6 @@
 cask "marq" do
-  version "1.2.4"
-  sha256 "bdb835878c5e9b01614e29df1fa3317ce7f92db9c74585c8840eacef7138bab1"
+  version "1.2.5"
+  sha256 "2618d509dd36f69d9e1e0c50035cbeff828a078c760d3eb0493b7945d6569b1f"
 
   url "https://github.com/jimbarritt/marq/releases/download/v#{version}/Marq.zip"
   name "Marq"
@@ -8,6 +8,15 @@ cask "marq" do
   homepage "https://github.com/jimbarritt/marq"
 
   app "Marq.app"
+
+  caveats <<~EOS
+    Marq is not yet notarised by Apple. If macOS reports that the app is
+    "damaged and can't be opened", clear the quarantine flag:
+
+      xattr -dr com.apple.quarantine "#{appdir}/Marq.app"
+
+    Then reopen Marq.
+  EOS
 
   zap trash: [
     "~/Library/Caches/com.jimbarritt.marq",
